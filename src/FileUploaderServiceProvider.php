d30c137226000369fc0f9f0721b1af665caf3cf9
Isaac Campos <isaaccamrod@gmail.com>
2019-09-11 20:37:38 +0200

    Add files via upload

---
+++ src/FileUploaderServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace diezcode\fileuploader;
+
+use Illuminate\Support\ServiceProvider;
+
+class FileUploaderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'app/FileUploader/FileUploader.php';
+    }
+}
